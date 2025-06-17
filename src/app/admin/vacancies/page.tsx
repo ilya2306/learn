6a519ccfc9142166cdd2dce384b8d19014dadb2e
Ilya Sokolov <imsokoloviv.w@gmail.com>
2025-06-17 09:35:25 +0300

    chore: Improvements

--- src/app/admin/vacancies/page.tsx
+++ src/app/admin/vacancies/page.tsx
@@ -1,10 +1,12 @@
+"use client";
 import Link from "next/link";
+import React, {useEffect} from "react";
 
 const getVacancies = async (): Promise<Response | undefined> => {
     try {
         const response = await fetch('https://learn-9fc9.vercel.app/api/vacancies/list')
 
-        return  await response.json();
+        return await response.json();
     } catch (error) {
         console.error(error);
     }
@@ -36,9 +38,13 @@ type Vacancy = {
     url: string
 }
 
-export default async function Vacancies() {
-    const vacancies = await getVacancies();
-    console.log(vacancies);
+export default function Vacancies() {
+    const [vacancies, setData] = React.useState<Response | null>(null);
+
+    useEffect(function () {
+        getVacancies()
+            .then((response) => setData(response ?? null))
+    }, [])
 
     return (
         <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
